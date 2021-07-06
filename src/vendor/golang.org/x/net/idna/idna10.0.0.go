98755550d18527a14d9083c93e0e2bd73cf6bc88
Steeve Morin <steeve@zen.ly>
2021-07-06 11:33:45 +0200

    [zenly1.17] make omitidna compatible with Go 1.17
    Signed-off-by: Steeve Morin <steeve@zen.ly>

--- src/vendor/golang.org/x/net/idna/idna10.0.0.go
+++ src/vendor/golang.org/x/net/idna/idna10.0.0.go
@@ -4,7 +4,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build go1.10
+//go:build go1.10 && !omitidna
 // +build go1.10
 // +build !omitidna
 
